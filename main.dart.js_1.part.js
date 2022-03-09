0887433655ea58aeecdfc0d88780125d0b5c3431
Rafi Magsi <muhammed.rafi@taifco.net>
2022-03-09 22:35:30 +0400

    Updated appost

--- main.dart.js_1.part.js
+++ main.dart.js_1.part.js
@@ -14,9 +14,9 @@ if(e.I(s).f.a.a>1366){x=3
 w=J.t4(x,y.e)
 for(s=this.r,v=0;v<x;++v){u=s[v]
 w[v]=new A.n_(1,B.e6,u,t)}s=A.a6(w,B.S,B.aS,B.r,t)}else s=A.ay(this.r,B.S,B.i,B.r,B.p)
-return A.B(t,s,B.f,t,t,t,t,t,t,t,new A.x(r,0,r,0),t,t,t)}}
+return A.B(t,s,B.f,t,t,t,t,t,t,t,new A.y(r,0,r,0),t,t,t)}}
 var z=a.updateTypes([]);(function inheritance(){var x=a.inherit
 x(C.Rw,A.X)})()
 A.aXi(b.typeUniverse,JSON.parse('{"Rw":{"X":[],"f":[]}}'))
 var y={h:A.ab("hr"),e:A.ab("f")}}
-$__dart_deferred_initializers__["l88ZYhdAz/Ci1QStAa/gAesggJ0="] = $__dart_deferred_initializers__.current
+$__dart_deferred_initializers__["zwCHW5fCA0BpNyvDgvk1UESiySA="] = $__dart_deferred_initializers__.current
