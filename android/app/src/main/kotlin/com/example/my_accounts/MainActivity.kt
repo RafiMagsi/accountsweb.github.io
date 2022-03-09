a55442146152c5c9ff8cdd8a651988ef16a71b05
Rafi Magsi <muhammed.rafi@taifco.net>
2022-03-09 21:36:36 +0400

    Add web

--- android/app/src/main/kotlin/com/example/my_accounts/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.my_accounts
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- android/app/src/main/kotlin/com/example/myaccountsapp/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.myaccountsapp
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
