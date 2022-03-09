8959fe1d1d3d84773054b3a7d57a0589745a984d
Rafi Magsi <muhammed.rafi@taifco.net>
2022-03-09 22:49:34 +0400

    Updated appost

--- main.dart.js_1.part.js
+++ main.dart.js_1.part.js
@@ -1,22 +1,22 @@
 self.$__dart_deferred_initializers__=self.$__dart_deferred_initializers__||Object.create(null)
 $__dart_deferred_initializers__.current=function(a,b,c,$){var C={
-b6Y(d,e){return new C.Rw(d,null)},
-Rw:function Rw(d,e){this.r=d
+b1X(d,e){return new C.PI(d,null)},
+PI:function PI(d,e){this.r=d
 this.a=e}},J,A,B
 a.setFunctionNamesIfNecessary([C])
 C=a.updateHolder(c[3],C)
 J=c[1]
 A=c[0]
 B=c[2]
-C.Rw.prototype={
+C.PI.prototype={
 B(d,e){var x,w,v,u,t=null,s=y.h,r=e.I(s).f.a.a<768?8:24
 if(e.I(s).f.a.a>1366){x=3
-w=J.t4(x,y.e)
+w=J.ro(x,y.e)
 for(s=this.r,v=0;v<x;++v){u=s[v]
-w[v]=new A.n_(1,B.e6,u,t)}s=A.a6(w,B.S,B.aS,B.r,t)}else s=A.ay(this.r,B.S,B.i,B.r,B.p)
-return A.B(t,s,B.f,t,t,t,t,t,t,t,new A.y(r,0,r,0),t,t,t)}}
+w[v]=new A.mx(1,B.dI,u,t)}s=A.a5(w,B.Q,B.aR,B.q,t)}else s=A.ay(this.r,B.Q,B.i,B.q,B.o)
+return A.A(t,s,B.f,t,t,t,t,t,t,t,new A.x(r,0,r,0),t,t,t)}}
 var z=a.updateTypes([]);(function inheritance(){var x=a.inherit
-x(C.Rw,A.X)})()
-A.aXi(b.typeUniverse,JSON.parse('{"Rw":{"X":[],"f":[]}}'))
-var y={h:A.ab("hr"),e:A.ab("f")}}
-$__dart_deferred_initializers__["zwCHW5fCA0BpNyvDgvk1UESiySA="] = $__dart_deferred_initializers__.current
+x(C.PI,A.X)})()
+A.aSZ(b.typeUniverse,JSON.parse('{"PI":{"X":[],"f":[]}}'))
+var y={h:A.ah("h7"),e:A.ah("f")}}
+$__dart_deferred_initializers__["i1djgIDvea7eVsUn9j1zjiIpiQE="] = $__dart_deferred_initializers__.current
